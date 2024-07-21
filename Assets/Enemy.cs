fc81ea2141ad55a71b6ab50f5d02ef594e43191b
Tasostagtal <tasostagtal@onskull.com>
2024-07-21 13:27:39 +0300

    enemy follows and attacks, line renderer on enemy isnt working for some reason

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,14 +4,57 @@ using UnityEngine;
 
 public class Enemy : Stats
 {
+    Rigidbody2D rb;
+    LineRenderer lineRenderer;
+    public LayerMask playerLayer;
     public override void Start()
     {
-        base.Start();   
+        base.Start();
+        rb = GetComponent<Rigidbody2D>();   
+        lineRenderer = GetComponent<LineRenderer>();
+        StartCoroutine(ChasePlayer());
     }
 
     public override void Die()
     {
         base.Die();
         gameObject.SetActive(false);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+    
+
+    IEnumerator ChasePlayer()
+    {
+        while(!IsDead)
+        {
+            yield return new WaitForSeconds(0.1f);
+            Vector3 target = gameManager.player.transform.position;
+            Vector2 direction = (target - transform.position).normalized;
+            if(Vector2.Distance(transform.position,gameManager.player.transform.position) > AttackRange)
+            {
+                rb.velocity = direction * moveSpeed;
+                lineRenderer.enabled = false;
+            }
+            else
+            {
+                rb.velocity = new Vector2();
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, AttackRange, playerLayer);
+
+        
+                if(hit.collider != null)
+                {
+                    lineRenderer.enabled = true;
+                    lineRenderer.SetPosition(0, transform.position);
+                    lineRenderer.SetPosition(1, target);
+                    //Debug.Log("Raycast hit: " + hit.collider.name);
+                    hit.collider.GetComponentInParent<Stats>().ModifyHealth(-damage);
+                    ModifyHealth(damage);
+                }  
+            }
+        }
     }
 }
--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -15,27 +15,19 @@ public class Stats : MonoBehaviour
     GameManager _gameManager;
     public int baseShadowEssence;
     public int ShadowEssence;
-
     public static int maxShadowEssence = 9999;
-
     public bool invincible;
-
     public bool invisible;
-
     public float moveSpeed;
-
     private SpriteRenderer spriteRenderer;
-
     private float invisibleAlpha = 0.5f;
     private float visibleAlpha = 1f;
-
     private Color initialColor;
     private float colorChangeSpeed = 1.0f;
-
     private float rainbowTime;
-
     public int damage;
-
+    public int AttackRange;
+    public bool IsDead = false;
     public virtual void Start()
     {
         ShadowEssence = baseShadowEssence;
@@ -58,7 +50,7 @@ public class Stats : MonoBehaviour
     }
 
 
-    void Update()
+    public virtual void Update()
     {
         RainbowEffect();
     }
@@ -101,6 +93,7 @@ public class Stats : MonoBehaviour
     }
     public virtual void Die()
     {
+        IsDead = true;
         gameManager.SpawnEssence(transform.position,ShadowEssence);
     }
 }
